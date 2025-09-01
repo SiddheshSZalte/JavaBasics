ab87efe2496dcd2b73db5293ec924be4644e5655
Siddhesh Zalte <siddheshzalte9370@gmail.com>
2025-09-01 21:46:49 +0530

    basic codes

---
+++ JavaBasics/src/javaRevision/Datatypes.java
@@ -0,0 +1,30 @@
+package javaRevision;
+
+public class Datatypes {
+public static void main(String args[]) {
+	int a = 1;// 4 bytes
+	String b = "siddhesh";
+	byte c = 127;// 1 byte
+	short d = 178;// 2 bytes
+	char e = 'e';// 2 bytes
+	boolean f = false;// 1 byte
+	int g = 12684;   // 4 bytes
+	long h = 269838063;// 8 bytes
+	float i = 1.7f; //4 bytes
+	double j = 1.7835; // 8 bytes
+	
+	 System.out.println(a);
+	 System.out.println(b);
+	 System.out.println(c);
+	 System.out.println(d);
+	 System.out.println(e);
+	 System.out.println(f);
+	 System.out.println(g);
+	 System.out.println(h);
+	 System.out.println(i);
+	 System.out.println(j);
+		
+	
+}
+
+}
