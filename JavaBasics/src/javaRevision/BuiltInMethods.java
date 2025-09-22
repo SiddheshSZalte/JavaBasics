98dec86072dad83bd495e3a96bf8e7ea0bf244ad
Siddhesh Zalte <siddheshzalte9370@gmail.com>
2025-09-22 20:00:22 +0530

    MathMethods

---
+++ JavaBasics/src/javaRevision/BuiltInMethods.java
@@ -0,0 +1,11 @@
+package javaRevision;
+
+public class BuiltInMethods {
+public static void main(String args[]) {
+	System.out.println(Math.sqrt(25));
+	System.out.println(Math.sqrt(24));
+	System.out.println(Math.floor(2.5));// rounds Down the value
+	System.out.println(Math.ceil(2.3));// rounds Up the value
+	//all of them stores values in doubles 
+}
+}
